6047bfb6a2871bf0c995b851600443ed011eb9b9
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-04 18:43:20 -0500

    GradeAvg update. Done.

--- Lab1.exercise2/src/GradeAvg.java
+++ Lab1.exercise2/src/GradeAvg.java
@@ -3,26 +3,27 @@ public class GradeAvg {
 
 	public static void main(String[] args) {
 	
-	double quizScore1 = 96f;
-	System.out.println("Quiz Score 1 = "+ quizScore1 +"");
+	double quizScore1 = 96;
+	System.out.println("Quiz Score 1 = "+ quizScore1 +""+ "%" +"");
 	
-	double quizScore2 = 55f;
-	System.out.println("Quiz Score 2 = "+ quizScore2 +"");
+	double quizScore2 = 55;
+	System.out.println("Quiz Score 2 = "+ quizScore2 +""+ "%" +"");
 	
-	double quizScore3 = 77f;
-	System.out.println("Quiz Score 3 = "+ quizScore3 +"");
+	double quizScore3 = 77;
+	System.out.println("Quiz Score 3 = "+ quizScore3 +""+ "%" +"");
 	
-	double testScore1 = 65f;
-	System.out.println("Test Score 1 = "+ quizScore1 +"");
+	double testScore1 = 65;
+	System.out.println("Test Score 1 = "+ quizScore1 +""+ "%" +"");
 	
-	double testScore2 = 99f;
-	System.out.println("Test Score 2 = "+ quizScore2 +"");
+	double testScore2 = 99;
+	System.out.println("Test Score 2 = "+ quizScore2 +""+ "%" +"");
 	
 	double quizScoreAvg = ((quizScore1 + quizScore2 + quizScore3) / 3);
-	System.out.println("Quiz Average = "+ quizScoreAvg +"");
+	System.out.println("Quiz Average = "+ quizScoreAvg +""+ "%" +"");
 	
 	double testScoreAvg = ((testScore1 + testScore2) / 2);
-	System.out.println("Test Average = "+ testScoreAvg +"");
+	System.out.println("Test Average = "+ testScoreAvg +""+ "%" +"");
+	
 
 	}
 
