080cc735a1570df7fb08a2b0d3eb568f0df8363a
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-04 18:25:16 -0500

    Second Commit. Updated GradeAvg

--- Lab1.exercise2/src/GradeAvg.java
+++ Lab1.exercise2/src/GradeAvg.java
@@ -5,6 +5,24 @@ public class GradeAvg {
 	
 	double quizScore1 = 96f;
 	System.out.println("Quiz Score 1 = "+ quizScore1 +"");
+	
+	double quizScore2 = 55f;
+	System.out.println("Quiz Score 2 = "+ quizScore2 +"");
+	
+	double quizScore3 = 77f;
+	System.out.println("Quiz Score 3 = "+ quizScore3 +"");
+	
+	double testScore1 = 65f;
+	System.out.println("Test Score 1 = "+ quizScore1 +"");
+	
+	double testScore2 = 99f;
+	System.out.println("Test Score 2 = "+ quizScore2 +"");
+	
+	double quizScoreAvg = ((quizScore1 + quizScore2 + quizScore3) / 3);
+	System.out.println("Quiz Average = "+ quizScoreAvg +"");
+	
+	double testScoreAvg = ((testScore1 + testScore2) / 2);
+	System.out.println("Test Average = "+ testScoreAvg +"");
 
 	}
 
