bf044dea5c0e8c8b2f0238c388615f9f96f2ef09
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-04 21:58:18 -0500

    Final commit

--- Lab1.exercise4/src/TimeComp.java
+++ Lab1.exercise4/src/TimeComp.java
@@ -8,13 +8,13 @@ public class TimeComp {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Enter a number of seconds that you want converted:");
+		System.out.print("Enter a number of seconds that you want converted:"); //prompts the user to input an amount of seconds they want converted
 		totalSeconds = scan.nextInt();
 		
 		
 		int hours = totalSeconds / 3600;
-		int minutes= (totalSeconds%3600 - totalSeconds%3600%60) / 60;
-		int seconds = totalSeconds%3600%60;
+		int minutes= (totalSeconds%3600 - totalSeconds%3600%60) / 60; //subtracts the hours so it doesnt return entire totalSeconds in only minutes
+		int seconds = totalSeconds%3600%60; 
 		
 		
 		System.out.print(+totalSeconds+ " seconds is equivalent to: " + hours + " hours, "+ minutes +" minutes, and "+ seconds+" seconds.");
