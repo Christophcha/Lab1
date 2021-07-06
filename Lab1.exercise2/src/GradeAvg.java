a82654286cd9bdc8013fce5f1d347776856805c6
Christophcha <39505761+Christophcha@users.noreply.github.com>
2021-07-06 16:53:55 -0500

    percentageFormat update.

--- Lab1.exercise2/src/GradeAvg.java
+++ Lab1.exercise2/src/GradeAvg.java
@@ -1,29 +1,31 @@
+import java.text.NumberFormat;
 
 public class GradeAvg {
 
 	public static void main(String[] args) {
 	
-	double quizScore1 = 96;
-	System.out.println("Quiz Score 1 = "+ quizScore1 +""+ "%" +"");
-	
-	double quizScore2 = 55;
-	System.out.println("Quiz Score 2 = "+ quizScore2 +""+ "%" +"");
-	
-	double quizScore3 = 77;
-	System.out.println("Quiz Score 3 = "+ quizScore3 +""+ "%" +"");
-	
-	double testScore1 = 65;
-	System.out.println("Test Score 1 = "+ quizScore1 +""+ "%" +"");
-	
-	double testScore2 = 99;
-	System.out.println("Test Score 2 = "+ quizScore2 +""+ "%" +"");
-	
-	double quizScoreAvg = ((quizScore1 + quizScore2 + quizScore3) / 3);
-	System.out.println("Quiz Average = "+ quizScoreAvg +""+ "%" +"");
-	
-	double testScoreAvg = ((testScore1 + testScore2) / 2);
-	System.out.println("Test Average = "+ testScoreAvg +""+ "%" +"");
-	
+		NumberFormat percentageFormat = NumberFormat.getPercentInstance();
+		
+		double quizScore1 = .96;
+		System.out.println("Quiz Score 1 = " + percentageFormat.format(quizScore1) +"");
+		
+		double quizScore2 = .55;
+		System.out.println("Quiz Score 2 = " + percentageFormat.format(quizScore2) +"");
+		
+		double quizScore3 = .77;
+		System.out.println("Quiz Score 3 = " + percentageFormat.format(quizScore3) +"");
+		
+		double testScore1 = .65;
+		System.out.println("Test Score 1 = " + percentageFormat.format(testScore1) +"");
+		
+		double testScore2 = .99;
+		System.out.println("Test Score 2 = " + percentageFormat.format(testScore2) +"");
+		
+		double quizScoreAvg = ((quizScore1 + quizScore2 + quizScore3) / 3);
+		System.out.println("Quiz Average = "+ percentageFormat.format(quizScoreAvg) +"");
+				
+		double testScoreAvg = ((testScore1 + testScore2) / 2);
+		System.out.println("Test Average = "+ percentageFormat.format(testScoreAvg) +"");
 
 	}
 
